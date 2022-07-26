5e10b7cfff9b2a0077089bc41dddd42de14dd634
mitchellabrouwer <mbarron198@gmail.com>
2022-07-26 21:35:36 +1000

    chore: linting clashes

--- lib/data.ts
+++ lib/data.ts
@@ -58,3 +58,29 @@ export const getPost = async (id, prisma) => {
 
   return post;
 };
+
+export const getVotes = async (post, prisma) => {
+  const upvotes = await prisma.vote.count({
+    where: { postId: post, up: true },
+  });
+
+  const downvotes = await prisma.vote.count({
+    where: { postId: post, up: false },
+  });
+
+  return upvotes - downvotes;
+};
+
+// eslint-disable-next-line camelcase
+export const getVote = async (post_id, user_id, prisma) => {
+  const vote = await prisma.vote.findMany({
+    // eslint-disable-next-line camelcase
+    where: { postId: post_id, authorId: user_id },
+  });
+
+  if (vote.length === 0) {
+    return null;
+  }
+
+  return vote[0];
+};
